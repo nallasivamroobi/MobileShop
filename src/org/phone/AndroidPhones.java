d5bb6add283714b3f855ccf6ce45d90ebd6c9b3b
nallasivamroobi <nallasivam712@gmail.com>
2021-09-12 23:49:05 +0530

    first version update

---
+++ src/org/phone/AndroidPhones.java
@@ -0,0 +1,17 @@
+package org.phone;
+
+public class AndroidPhones {
+
+	public void HonorPhones()
+	{
+		System.out.println("=======================");
+		System.out.println("honor phones");
+	}
+	
+	
+	public void AppoPhones()
+	{	
+		System.out.println("Appo phones");
+	}
+	
+}
